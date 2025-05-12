7173ebf905317a786b506328cbb415d2f014664c
Miriam Rawlins <miriammrawlins@gmail.com>
2025-05-12 13:16:22 -0600

    adding an update and create feature; fixing bugs

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ app.get("/filtered", (req, res) => {
       console.log("Error reading file", err);
       return;
     }
-    const rawCardData = JSON.parse(data);
-    const cardData = rawCardData.cards;
+    /* const rawCardData = JSON.parse(data);
+    const cardData = rawCardData.cards; */
+    const cardData = JSON.parse(data);
 
     const { id, name, set, cardNumber, type, power, toughness, rarity, cost } =
       req.query;
@@ -92,6 +93,129 @@ app.get("/filtered", (req, res) => {
     }
 
     res.json(filteredCards);
+  });
+});
+
+/* app.put("/card/:id", (req, res) => {
+  fs.readFile("cards.json", "utf8", (err, data) => {
+    if (err) {
+      console.log("Error reading file", err);
+      return;
+    }
+
+    const rawCardData = JSON.parse(data);
+    const cardData = rawCardData.cards;
+
+    const cardId = req.params.id;
+    const cardIndex = cardData.findIndex(
+      (card) => card.id === parseInt(cardId)
+    );
+
+    if (cardIndex === -1) {
+      return res.status(404).send("Card not found");
+    }
+
+    cardData[cardIndex] = {
+      ...cardData[cardIndex],
+      ...req.body,
+    };
+
+    rawCardData.cards = cardData;
+    fs.writeFile("cards.json", JSON.stringify(rawCardData, null, 2), (err) => {
+      if (err) {
+        console.log("Error writing file", err);
+        return res.status(500).send("Error updating card");
+      }
+
+      res.json(cardData[cardIndex]);
+    });
+  });
+}); */
+
+app.get("/update", (req, res) => {
+  fs.readFile("cards.json", "utf8", (err, data) => {
+    if (err) {
+      console.log("Error reading file", err);
+      return res.status(500).send("Error reading file");
+    }
+
+    /* const rawCardData = JSON.parse(data);
+    const cardData = rawCardData.cards; */
+
+    const cardData = JSON.parse(data);
+
+    const { id, name, set, cardNumber, type, power, toughness, rarity, cost } =
+      req.query;
+
+    if (!id) {
+      return res.status(404).send("Card ID is required");
+    }
+    console.log(cardData);
+
+    const cardIndex = cardData.findIndex((card) => card.id === parseInt(id));
+
+    if (cardIndex === -1) {
+      return res.status(404).send("Card not found");
+    }
+
+    if (name) cardData[cardIndex].name = name;
+    if (set) cardData[cardIndex].set = set;
+    if (cardNumber) cardData[cardIndex].cardNumber = cardNumber;
+    if (type) cardData[cardIndex].type = type;
+    if (power) cardData[cardIndex].power = power;
+    if (toughness) cardData[cardIndex].toughness = toughness;
+    if (rarity) cardData[cardIndex].rarity = rarity;
+    if (cost) cardData[cardIndex].cost = cost;
+
+    fs.writeFile("cards.json", JSON.stringify(cardData, null, 2), (err) => {
+      if (err) {
+        console.log("Error writing file", err);
+        return res.status(500).send("Error updating card");
+      }
+
+      res.json(cardData[cardIndex]);
+    });
+  });
+});
+
+app.get("/create", (req, res) => {
+  fs.readFile("cards.json", "utf8", (err, data) => {
+    if (err) {
+      console.log("Error reading file", err);
+      return res.status(500).send("Error reading file");
+    }
+
+    /* const rawCardData = JSON.parse(data);
+    const cardData = rawCardData.cards; */
+    const cardData = JSON.parse(data);
+
+    const { name, set, cardNumber, type, power, toughness, rarity, cost } =
+      req.query;
+
+    if (name || set || type || power || toughness || rarity || cost) {
+      const newCard = {
+        id: cardData.length + 2,
+        name: name,
+        set: set,
+        cardNumber: cardNumber,
+        type: type,
+        power: power,
+        toughness: toughness,
+        rarity: rarity,
+        cost: cost,
+      };
+      cardData.push(newCard);
+
+      const updateCardsJson = JSON.stringify(cardData, null, 2);
+      fs.writeFile("cards.json", updateCardsJson, "utf8", (err) => {
+        if (err) {
+          console.error("Failed to write:", err);
+          rerturn;
+        }
+        console.log("File written");
+        res.send("Card created", newCard);
+      });
+    }
   });
 });
 
